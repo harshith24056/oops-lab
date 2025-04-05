47b814480adba1435c612a0a44144b4308d7dd00
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 15:44:45 +0530

    Create single Inheritance-2.java

---
+++ INHERITANCE/single Inheritance-2.java
@@ -0,0 +1,20 @@
+class Student {
+    void Fee() {
+        System.out.println("Student Fee = 20000");
+    }
+}
+
+class Student Name extends Student { 
+    void Name () {
+        System.out.println("Student Name = Jayanti");
+    }
+}
+
+public class SingleInheritance1 {
+    public static void main (String args[]) {
+        Student Name p = new Student Name ();
+        p.Fee(); 
+        p.Name(); 
+    }
+}
+
