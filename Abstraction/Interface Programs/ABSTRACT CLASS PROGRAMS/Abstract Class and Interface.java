df79e596612b6b0d90940aa93fed296508e21cbd
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 16:39:33 +0530

    Create Abstract Class and Interface.java

---
+++ Abstraction/Interface Programs/ABSTRACT CLASS PROGRAMS/Abstract Class and Interface.java
@@ -0,0 +1,38 @@
+interface A {
+    void a(); 
+    void b(); 
+    void c(); 
+    void d(); 
+}
+abstract class B implements A {
+    public void c() {
+        System.out.println("I am c");
+    }
+}
+
+class M extends B {
+    
+    public void a() {
+        System.out.println("I am a");
+    }
+
+    public void b() {
+        System.out.println("I am b");
+    }
+
+    
+    public void d() {
+        System.out.println("I am d");
+    }
+}
+
+public class Abstract {
+    public static void main(String args[]) {
+        A a = new M(); // Create an instance of M
+        a.a(); 
+        a.b(); 
+        a.c(); 
+        a.d(); 
+}
+}
+
