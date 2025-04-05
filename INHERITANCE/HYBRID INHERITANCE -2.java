c13df71792f5a69c6c776fa2dc08b19083607e25
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 15:55:34 +0530

    Create HYBRID INHERITANCE -2.java

---
+++ INHERITANCE/HYBRID INHERITANCE -2.java
@@ -0,0 +1,30 @@
+class HumanBody {
+    public void displayHuman() {
+        System.out.println("Method defined inside HumanBody class");
+    }
+}
+
+interface Male {
+    public void show();
+}
+
+interface Female {
+    public void show();
+}
+
+public class HybridInheritance extends HumanBody implements Male, Female {
+    public void show() {
+        System.out.println("Implementation of show() method defined in interfaces Male and Female");
+    }
+
+    public void displayHybrid() {
+        System.out.println("Method defined inside HybridInheritance class");
+    }
+
+    public static void main(String[] args) {
+        HybridInheritance obj = new HybridInheritance();
+        System.out.println("Implementation of Hybrid Inheritance in Java");
+        obj.show();          
+        obj.displayHybrid(); 
+        obj.displayHuman();  
+}
