0d7cefe838fb88a4ca1b42d74096304d501c37ad
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 16:35:58 +0530

    Create BikeAbstract.java

---
+++ Abstraction/Interface Programs/ABSTRACT CLASS PROGRAMS/BikeAbstract.java
@@ -0,0 +1,26 @@
+abstract class MotorBike {
+    abstract void brake(); 
+}
+
+class SportsBike extends MotorBike {
+    public void brake() {
+        System.out.println("SportsBike Brake");
+    }
+}
+
+class MountainBike extends MotorBike {
+    
+    public void brake() {
+        System.out.println("MountainBike Brake");
+    }
+}
+
+public class Abstract {
+    public static void main(String[] args) {
+        MountainBike m1 = new MountainBike(); 
+        m1.brake(); 
+
+        SportsBike s1 = new SportsBike(); 
+        s1.brake(); 
+    }
+}
