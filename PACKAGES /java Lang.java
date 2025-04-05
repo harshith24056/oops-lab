3c7f3746722b3e6302ee5c786403a87cc06dd9cd
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 16:49:57 +0530

    Create java Lang.java

---
+++ PACKAGES /java Lang.java
@@ -0,0 +1,9 @@
+class JavaLangExample {
+    public static void main(String args[]) {
+        int a = 20, b = 30; 
+        int sum = Math.addExact(a, b); 
+        int max = Math.max(a, b); 
+        double pi = Math.PI; 
+        System.out.printf("Sum = %d, Max = %d, PI = %.2f%n", sum, max, pi);
+    }
+}
