457909d0bc2bf6c7fcbb9cef6b5ec8c818bffd08
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 16:37:08 +0530

    Create AbstractShape.java

---
+++ Abstraction/Interface Programs/ABSTRACT CLASS PROGRAMS/AbstractShape.java
@@ -0,0 +1,28 @@
+abstract class Shape {
+    abstract void draw(); 
+    abstract double area(); 
+}
+
+class Circle extends Shape {
+    private double radius; 
+    
+    Circle(double radius) {
+        this.radius = radius;
+    }
+    void draw() {
+        System.out.println("Drawing Circle");
+   
+}
+    double area() {
+        return Math.PI * radius * radius; 
+    }
+}
+
+public class MultipleAbstract {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5); 
+        circle.draw(); 
+        System.out.println("Area: " + circle.area()); 
+    }
+}
+
