9ba9b520fde9c6d8d3fb778e35c118fa86baebe2
harshith24056 <nelluriharshith2007@gmail.com>
2025-04-05 16:38:42 +0530

    Create Bank Rate of Interest.java

---
+++ Abstraction/Interface Programs/ABSTRACT CLASS PROGRAMS/Bank Rate of Interest.java
@@ -0,0 +1,26 @@
+abstract class Bank {
+    abstract int getRateOfInterest(); 
+}
+
+class SBI extends Bank {
+    
+    int getRateOfInterest() {
+        return 7; 
+    }
+}
+    class PNB extends Bank 
+    int getRateOfInterest() {
+        return 8; 
+    }
+}
+public class Abstract {
+    public static void main(String args[]) {
+        Bank b; 
+
+        b = new SBI(); 
+        System.out.println("Rate of Interest is: " + b.getRateOfInterest() + " %"); 
+
+        b = new PNB(); 
+        System.out.println("Rate of Interest is: " + b.getRateOfInterest() + " %"); 
+    }
+}
